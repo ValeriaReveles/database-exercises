8bf048e80be0818779be68536c61ef9d2862db2b
Valeria <valeriareveles12@gmail.com>
2022-08-30 14:15:57 -0500

    completed limit exercises in mysql

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -6,6 +6,7 @@ USE employees;
 SELECT DISTINCT last_name FROM employees
 ORDER BY last_name DESC;
 
+
 # Create a query to get the top 5 salaries and display just the employees number from the salaries table. The employee number results should be:
 SELECT emp_no FROM salaries
 ORDER BY salary DESC
