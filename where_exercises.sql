8d298effd1611afd6dd525c4d80c9be4082859de
Valeria <valeriareveles12@gmail.com>
2022-08-30 09:34:14 -0500

    completed part one of where sql exercise

---
+++ where_exercises.sql
@@ -0,0 +1,19 @@
+-- Part One:
+USE employees;
+
+-- Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+-- Find all employees whose last name starts with 'E' — 7,330 rows.
+SELECT last_name, first_name
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY last_name ;
+
+-- Find all employees with a 'q' in their last name — 1,873 rows.
+SELECT last_name, first_name
+FROM employees
+WHERE last_name LIKE '%q%'
+ORDER BY last_name;
