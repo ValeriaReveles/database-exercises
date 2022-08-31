7fef28f5b8560e69fb23dfdcb0b4b5142e1c002b
Valeria <valeriareveles12@gmail.com>
2022-08-31 11:01:29 -0500

    completed group-by exercises, updated functions on mySQL

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,7 +1,8 @@
 
 USE employees;
--- # Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
 
+
+-- # Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
 SELECT CONCAT(first_name, ' ', last_name) AS Name
 FROM employees
 WHERE (last_name LIKE 'e%'
@@ -24,7 +25,6 @@ WHERE year(hire_date) BETWEEN 1990 AND 1999
 ORDER BY hire_date;
 
 -- # Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
-
 SELECT *
 FROM employees
 WHERE year(hire_date) BETWEEN 1990 AND 1999
@@ -33,9 +33,8 @@ WHERE year(hire_date) BETWEEN 1990 AND 1999
 ORDER BY hire_date DESC , birth_date ASC;
 
 -- # For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You might also need to use now() or curdate()).
-
 SELECT DATEDIFF(CURDATE(), hire_date),
-       CONCAT(CURDATE(), ' - ', hire_date) AS Employee_History,
+       CONCAT(CURDATE(), ' : ', hire_date) AS Employee_History,
        CONCAT(first_name, ' ', last_name) AS Employee_Name
 FROM employees
 WHERE year(hire_date) BETWEEN 1990 AND 1999
