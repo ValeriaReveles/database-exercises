b1f307f481573d77f0e5aa03a5fffb76961259ee
Valeria <valeriareveles12@gmail.com>
2022-09-02 09:49:29 -0500

    started join_exercises on mysql

---
+++ join_exercises.sql
@@ -0,0 +1,48 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS roles;
+
+CREATE TABLE roles (
+   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+   name VARCHAR(100) NOT NULL,
+   PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+   name VARCHAR(100) NOT NULL,
+   email VARCHAR(100) NOT NULL,
+   role_id INT UNSIGNED DEFAULT NULL,
+   PRIMARY KEY (id),
+   FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
+INSERT INTO roles (name) VALUES ('admin');
+INSERT INTO roles (name) VALUES ('author');
+INSERT INTO roles (name) VALUES ('reviewer');
+INSERT INTO roles (name) VALUES ('commenter');
+
+INSERT INTO users (name, email, role_id) VALUES
+     ('bob', 'bob@example.com', 1),
+     ('joe', 'joe@example.com', 2),
+     ('sally', 'sally@example.com', 3),
+     ('adam', 'adam@example.com', 3),
+     ('jane', 'jane@example.com', null),
+     ('mike', 'mike@example.com', null);
+
+
+-- Using the example in the Associative Table Joins section as a guide, write a query that shows each department along with the name of the current manager for that department.
+
+
+-- Find the name of all departments currently managed by women.
+
+
+
+
+-- Find the current titles of employees currently working in the Customer Service department.
+
+
+
+
+
+-- Find the current salary of all current managers.
+
